e04bb3ec7e67b67eacb920f1857401555e07d626
Levan <levan.ostrowski@codema.pl>
2022-02-10 16:22:58 +0100

    blind commit

---
+++ src/Modules/StrTransformer/StrTransformer.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Project: deep-insight-api.
+ * Author: Levan Ostrowski
+ * User: cod3venom
+ * Date: 10.02.2022
+ * Time: 09:15
+*/
+
+namespace App\Modules\StrTransformer;
+
+class StrTransformer
+{
+
+}
