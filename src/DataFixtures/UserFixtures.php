2e63ef99aadcd204389ba1dee04ce9abca9d3bde
Levan <levan.ostrowski@codema.pl>
2022-02-18 12:53:28 +0100

    added user fixture for Michał

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -16,6 +16,8 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $userid = 'f2881cd4-a02e-48dc-81bf-f1537a0b903f';
+        $michalUserId = Uuid::uuid4()->toString();
+
         $user = new User();
         $user->profile = new UserProfile();
         $user->company = new UserCompanyInfo();
@@ -33,6 +35,53 @@ class UserFixtures extends Fixture
             ->setLastName('Ostrowski')
             ->setBirthDay(new DateTime('1998-11-17'))
             ->setEmail('levan.ostrowski@gmail.com')
+            ->setPhone('514380928')
+            ->setCreatedAt();;
+
+        $user->company
+            ->setUserId($userid)
+            ->setCompanyName('Roots-Connector')
+            ->setCompanyWww('www.rootsconnector.com')
+            ->setCompanyIndustry('IT')
+            ->setWayToEarnMoney('Want more? do more !!!')
+            ->setRegon('12345')
+            ->setKrs('54321')
+            ->setNip('56789')
+            ->setDistricts('LA, Birmingham')
+            ->setHeadQuartersCity('England ....')
+            ->setBusinessEmails('bussiness@rootsconnector.com')
+            ->setBusinessPhones(
+                '514380928, '.
+                '514380929, '.
+                '514380930, '.
+                '514380931'
+            )
+            ->setRevenue('10000000')
+            ->setProfit('5000000')
+            ->setGrowthYearToYear('')
+            ->setCategories('IT, Programming, Big Data, AI, Cyber Security')
+            ->setCreatedAt();
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $user = new User();
+        $user->profile = new UserProfile();
+        $user->company = new UserCompanyInfo();
+        $user
+            ->setUserId($userid)
+            ->setEmail('michalwojda@gmail.com')
+            ->setPassword(password_hash('admin', PASSWORD_DEFAULT))
+            ->setRoles([User::ROLE_USER, User::ROLE_ADMIN])
+            ->setLastLoginAt()
+            ->setCreatedAt();
+
+        $user->profile
+            ->setUserId($userid)
+            ->setFirstName('Michał')
+            ->setLastName('Wojda')
+            ->setBirthDay(new DateTime('1998-11-17'))
+            ->setEmail('michalwojda@gmail.com')
             ->setPhone('514380928')
             ->setCreatedAt();;
 
